8837a583b7f4064ce085b9137020e029361f49b8
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-26 00:44:38 +0330

    Expose complete architecture contracts

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,2 +1,4 @@
 export type { ApplicationState, SessionState, NavigationState } from "./interfaces/application-state";
 export type { StateStore } from "./interfaces/state-store";
+export type { ShopperFlowState } from "./interfaces/shopper-flow-state";
+export type { CommerceState } from "./interfaces/commerce-state";
