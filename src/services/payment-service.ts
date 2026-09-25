34dd8ecf7851ef2eedaf487a70eac12cb688998a
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-25 23:27:00 +0330

    C1-C2: Add payment service

---
+++ src/services/payment-service.ts
@@ -0,0 +1,18 @@
+import type { PaymentProvider } from "../providers/interfaces/payment-provider";
+import type { Payment, PaymentId, OrderId } from "../../domain";
+
+export class PaymentService {
+  constructor(private readonly provider: PaymentProvider) {}
+
+  getPayment(id: PaymentId): Promise<Payment | null> {
+    return this.provider.getPayment(id);
+  }
+
+  createPayment(payment: Payment): Promise<Payment> {
+    return this.provider.createPayment(payment);
+  }
+
+  listPayments(orderId: OrderId): Promise<readonly Payment[]> {
+    return this.provider.listPayments(orderId);
+  }
+}
