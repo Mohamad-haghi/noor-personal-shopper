71ad46bd305bb26d9a6cc747a2a21e2f8bd31048
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-25 23:27:40 +0330

    C1-C2: Add architecture barrel exports

---
+++ src/providers/demo/index.ts
@@ -0,0 +1,6 @@
+export { DemoOrderProvider } from "./demo-order-provider";
+export { DemoPaymentProvider } from "./demo-payment-provider";
+export { DemoConfirmationProvider } from "./demo-confirmation-provider";
+export { DemoBranchProvider } from "./demo-branch-provider";
+export { DemoVisitProvider } from "./demo-visit-provider";
+export { DemoHeroProvider } from "./demo-hero-provider";
