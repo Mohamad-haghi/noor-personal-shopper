4e0ea40cf3501fb7c80b4a5e5f08a45f084ec8b9
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-25 23:25:57 +0330

    C1-C2: Add compare service

---
+++ src/services/compare-service.ts
@@ -0,0 +1,18 @@
+import type { CompareProvider } from "../providers/interfaces/compare-provider";
+import type { Comparison, ComparisonId } from "../../domain";
+
+export class CompareService {
+  constructor(private readonly provider: CompareProvider) {}
+
+  getComparison(id: ComparisonId): Promise<Comparison | null> {
+    return this.provider.getComparison(id);
+  }
+
+  createComparison(comparison: Comparison): Promise<Comparison> {
+    return this.provider.createComparison(comparison);
+  }
+
+  saveComparison(comparison: Comparison): Promise<Comparison> {
+    return this.provider.saveComparison(comparison);
+  }
+}
