c92ed4e32d6ecebaa139b75195f3295c3c4ed3f8
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-25 23:27:06 +0330

    C1-C2: Add confirmation service

---
+++ src/services/confirmation-service.ts
@@ -0,0 +1,18 @@
+import type { ConfirmationProvider } from "../providers/interfaces/confirmation-provider";
+import type { Confirmation, ConfirmationId, OrderId } from "../../domain";
+
+export class ConfirmationService {
+  constructor(private readonly provider: ConfirmationProvider) {}
+
+  getConfirmation(id: ConfirmationId): Promise<Confirmation | null> {
+    return this.provider.getConfirmation(id);
+  }
+
+  createConfirmation(confirmation: Confirmation): Promise<Confirmation> {
+    return this.provider.createConfirmation(confirmation);
+  }
+
+  getConfirmationForOrder(orderId: OrderId): Promise<Confirmation | null> {
+    return this.provider.getConfirmationForOrder(orderId);
+  }
+}
