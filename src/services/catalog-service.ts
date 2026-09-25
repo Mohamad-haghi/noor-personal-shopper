6c6a8a5dca4621d84ce23c3626edcebe9a174138
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-25 23:25:21 +0330

    C1-C2: Add catalog service

---
+++ src/services/catalog-service.ts
@@ -0,0 +1,22 @@
+import type { CatalogProvider } from "../providers/interfaces/catalog-provider";
+import type { Product, ProductVariant, ProductId, ProductVariantId } from "../../domain";
+
+export class CatalogService {
+  constructor(private readonly provider: CatalogProvider) {}
+
+  listProducts(): Promise<readonly Product[]> {
+    return this.provider.listProducts();
+  }
+
+  getProduct(id: ProductId): Promise<Product | null> {
+    return this.provider.getProduct(id);
+  }
+
+  listVariants(productId: ProductId): Promise<readonly ProductVariant[]> {
+    return this.provider.listVariants(productId);
+  }
+
+  getVariant(id: ProductVariantId): Promise<ProductVariant | null> {
+    return this.provider.getVariant(id);
+  }
+}
