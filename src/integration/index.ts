e3c7c0e5b6b51b8d19bef108209d7ea19f5aa6c9
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-25 23:43:15 +0330

    C1-C3: Export integration contracts

--- src/integration/index.ts
+++ src/integration/index.ts
@@ -1,1 +1,11 @@
-export type { IntegrationProvider, ProductIntegration, ProductQuery, ShopperIntegration, OrderIntegration, PaymentIntegration, BranchIntegration } from "./interfaces/integration-provider";
+export type {
+  IntegrationProvider,
+  ProductIntegration,
+  ProductQuery,
+  ShopperIntegration,
+  OrderIntegration,
+  PaymentIntegration,
+  BranchIntegration,
+} from "./interfaces/integration-provider";
+export type { ShopperLaunchSource, ShopperHandoffContext, ShopperHandoffCodec, NoorIntegrationContext } from "./interfaces/shopper-handoff";
+export type { HeroContentProvider, HeroDestinationResolver } from "./interfaces/hero-integration";
