57efd94cf870ede9b7d2492accabd0652c58976f
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-25 23:25:33 +0330

    C1-C2: Add recommendations service

---
+++ src/services/recommendations-service.ts
@@ -0,0 +1,14 @@
+import type { RecommendationsProvider } from "../providers/interfaces/recommendations-provider";
+import type { Recommendation, RecommendationContext, ShopperProfileId } from "../../domain";
+
+export class RecommendationsService {
+  constructor(private readonly provider: RecommendationsProvider) {}
+
+  getRecommendations(shopperId: ShopperProfileId): Promise<readonly Recommendation[]> {
+    return this.provider.getRecommendations(shopperId);
+  }
+
+  generateRecommendations(shopperId: ShopperProfileId, context: RecommendationContext): Promise<readonly Recommendation[]> {
+    return this.provider.generateRecommendations(shopperId, context);
+  }
+}
