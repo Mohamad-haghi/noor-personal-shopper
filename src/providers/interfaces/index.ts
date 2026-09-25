48ea08cefb381811e539ee2258ebe29b4601a01a
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-25 23:27:27 +0330

    C1-C2: Add architecture barrel exports

---
+++ src/providers/interfaces/index.ts
@@ -0,0 +1,6 @@
+export type { OrderProvider } from "./order-provider";
+export type { PaymentProvider } from "./payment-provider";
+export type { ConfirmationProvider } from "./confirmation-provider";
+export type { BranchProvider } from "./branch-provider";
+export type { VisitProvider } from "./visit-provider";
+export type { HeroProvider } from "./hero-provider";
