fba3ab97acab95a98a0cb921c701776be7ce6596
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-26 00:44:59 +0330

    Expose complete integration boundaries

--- src/integration/index.ts
+++ src/integration/index.ts
@@ -9,3 +9,6 @@ export type {
 } from "./interfaces/integration-provider";
 export type { ShopperLaunchSource, ShopperHandoffContext, ShopperHandoffCodec, NoorIntegrationContext } from "./interfaces/shopper-handoff";
 export type { HeroContentProvider, HeroDestinationResolver } from "./interfaces/hero-integration";
+export type { SearchIntegration } from "./interfaces/search-integration";
+export type { IdentityIntegration } from "./interfaces/identity-integration";
+export type { ReservationIntegration } from "./interfaces/reservation-integration";
