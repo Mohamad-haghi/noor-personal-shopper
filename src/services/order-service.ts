d7820df7bba464e9e4b07ab96af2f96f3234c5bf
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-25 23:26:44 +0330

    C1-C2: Add order service

---
+++ src/services/order-service.ts
@@ -0,0 +1,18 @@
+import type { OrderProvider } from "../providers/interfaces/order-provider";
+import type { Order, OrderId, AccountId } from "../../domain";
+
+export class OrderService {
+  constructor(private readonly provider: OrderProvider) {}
+
+  getOrder(id: OrderId): Promise<Order | null> {
+    return this.provider.getOrder(id);
+  }
+
+  createOrder(order: Order): Promise<Order> {
+    return this.provider.createOrder(order);
+  }
+
+  listOrders(accountId: AccountId): Promise<readonly Order[]> {
+    return this.provider.listOrders(accountId);
+  }
+}
