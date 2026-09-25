5eafa7060f125c76c9f1d397b6f23ae2fbf42948
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-25 23:27:18 +0330

    C1-C2: Add visit service

---
+++ src/services/visit-service.ts
@@ -0,0 +1,18 @@
+import type { VisitProvider } from "../providers/interfaces/visit-provider";
+import type { Visit, VisitId } from "../../domain";
+
+export class VisitService {
+  constructor(private readonly provider: VisitProvider) {}
+
+  getVisit(id: VisitId): Promise<Visit | null> {
+    return this.provider.getVisit(id);
+  }
+
+  requestVisit(visit: Visit): Promise<Visit> {
+    return this.provider.requestVisit(visit);
+  }
+
+  cancelVisit(id: VisitId): Promise<Visit | null> {
+    return this.provider.cancelVisit(id);
+  }
+}
