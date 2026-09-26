89e58dbaa2bd1c646da4203526a0579bf0ece562
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-27 00:20:43 +0330

    feat(g4): export selection profile provider

--- src/providers/interfaces/index.ts
+++ src/providers/interfaces/index.ts
@@ -1,5 +1,6 @@
 export type { CatalogProvider } from "./catalog-provider";
 export type { ShopperProvider } from "./shopper-provider";
+export type { SelectionProfileProvider } from "./selection-profile-provider";
 export type { ChoicesProvider } from "./choices-provider";
 export type { CompareProvider } from "./compare-provider";
 export type { AccountProvider } from "./account-provider";
