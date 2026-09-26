691e26e9b32caf5da960b24f6cc958dfadf1c28a
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-26 15:21:48 +0330

    chore: export availability source type

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,5 +1,5 @@
 export type { ProductId, ProductSource, Product, ProductAttributes, ProductMerchandising, ProductMedia, ProductImage, ExternalProductIdentifiers } from "./product";
-export type { ProductVariantId, ProductVariant, VariantAttributes, VariantAvailability, AvailabilityStatus } from "./product-variant";
+export type { ProductVariantId, ProductVariant, VariantAttributes, VariantAvailability, AvailabilitySource, AvailabilityStatus } from "./product-variant";
 export type { ShopperProfileId, ShopperProfile, ShopperPreferences, NotificationPreferences, StyleProfile, FormalityLevel, MeasurementProfile, MeasurementUnit, BudgetProfile, PriceRangePreference } from "./shopper-profile";
 export type { SelectionProfileId, SelectionProfile, SelectionItem, SelectionPriority } from "./selection-profile";
 export type { RecommendationId, Recommendation, RecommendationScore, RecommendationReason, RecommendationPrimaryReason, RecommendationSecondaryReason, RecommendationContext, Season, RecommendationSource } from "./recommendation";
